47d51d1dee57af6c06e2c9a6dca75bb70c4f9809
Sidharth Sharma <sidharth14949@gmail.com>
2026-08-09 11:15:27 +0530

    Update app-context.tsx

--- src/lib/app-context.tsx
+++ src/lib/app-context.tsx
@@ -16,7 +16,8 @@ type AppPrefsValue = {
   t: (key: TKey) => string;
   dark: boolean;
   toggleDark: () => void;
-
+  kidMode: boolean;
+  toggleKidMode: () => void;
 };
 
 const AppPrefsContext = createContext<AppPrefsValue | null>(null);
